fed1cc320a38246dc68c14a120b0436ca49e0461
Navneet Sharma <navneetsharmaprogrammer@gmail.com>
2025-07-29 03:08:03 +0530

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -22,7 +22,16 @@ export const GOOGLE_SHEET_CSV_URL: string = 'https://docs.google.com/spreadsheet
 
 
 /**
- * 2. BRAND LOGO
+ * 2. GOOGLE APPS SCRIPT FOR ORDERS (Optional, but Recommended)
+ * -----------------------------------------------------------
+ * URL for your deployed Google Apps Script to handle automated order submissions.
+ * Instructions for setup are in USER_MANUAL.txt and in the ORDER_APPS_SCRIPT.js file.
+ * If left empty, this checkout method will be disabled.
+ */
+export const GOOGLE_APPS_SCRIPT_URL: string = ''; // <-- PASTE YOUR GOOGLE APPS SCRIPT WEB APP URL HERE
+
+/**
+ * 3. BRAND LOGO
  * -------------
  * The main logo for your website.
  * HOW TO SET UP:
@@ -33,7 +42,7 @@ export const GOOGLE_SHEET_CSV_URL: string = 'https://docs.google.com/spreadsheet
 export const LOGO_URL = 'https://drive.google.com/file/d/1wE7L2p5N_N-HwXyJ2K8qY7tQ8jZ9k5F4/view?usp=drive_link'; // <-- PASTE YOUR LOGO URL HERE (This is an example link)
 
 /**
- * 3. INSTAGRAM HANDLE
+ * 4. INSTAGRAM HANDLE
  * -------------------
  * Your store's Instagram handle, used for "Order via DM" links.
  * Do not include the '@' symbol.
@@ -41,7 +50,7 @@ export const LOGO_URL = 'https://drive.google.com/file/d/1wE7L2p5N_N-HwXyJ2K8qY7
 export const INSTAGRAM_HANDLE = 'thriftbymusk';
 
 /**
- * 4. NEXT DROP LAUNCH DATE
+ * 5. NEXT DROP LAUNCH DATE
  * ------------------------
  * Set the target date and time for your next product drop countdown timer.
  * FORMAT: 'YYYY-MM-DDTHH:MM:SSZ' (ISO 8601 format). 'Z' denotes UTC time.
